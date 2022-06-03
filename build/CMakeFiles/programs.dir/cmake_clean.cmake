6132c3dc8608dd8054bd98e3440cd9a4a6c817f1
kenonix <drone5023@gmail.com>
2022-06-03 18:56:46 +0900

    cmake is up!!

---
+++ build/CMakeFiles/programs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programs.dir/jsoncpp.cpp.o"
+  "CMakeFiles/programs.dir/main.cpp.o"
+  "programs"
+  "programs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
